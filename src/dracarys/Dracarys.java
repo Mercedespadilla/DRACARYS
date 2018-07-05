77bd6b92c4106237dac40145e83b4d35e35d8b02
Monica Gomez <Monica Gomez@192.168.2.5>
2018-07-04 23:29:46 -0600

    Plantillas, imagenes ppales

--- src/dracarys/Dracarys.java
+++ src/dracarys/Dracarys.java
@@ -3,9 +3,7 @@ package dracarys;
 
 public class Dracarys {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
